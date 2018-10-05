c85066b46702d7293ab31a8b95dd2003fea75266
Adam Romański <adam.romanski@netguru.pl>
2018-10-05 23:11:37 +0200

    Create basic structure with css modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-styled-components',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     `gatsby-plugin-react-svg`,
+    'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styled, { injectGlobal } from 'styled-components';
 import 'normalize.css';
-import Wrapper from '../components/Wrapper/Wrapper';
-import Navigation from '../components/Navigation/Navigation';
-
-injectGlobal`
-  html {
-    font-size: 62.5%;
-  }
-
-  body {
-    font-family: 'Montserrat', sans-serif;
-    font-size: 1.6rem;
-    line-height: 1.62;
-    background-color: #F8F8F8;
-    color: #1D1D1D;
-  }
-
-  *, *:after, *:before {
-    box-sizing: border-box;
-  }
-`;
-
-const LogoWrapper = styled.div`
-  width: 100%;
-  height: auto;
-  
-  svg {
-    width: 100%;
-    height: auto;
-  }
-`;
+import './global.scss';
+import styles from './index.module.scss';
 
 export default props => (
-  <Wrapper>
+  <div>
     <Helmet>
       <html lang="en" />
       <title>
@@ -45,6 +16,6 @@ export default props => (
         content="Some content."
       />
     </Helmet>
-    <Navigation />
-  </Wrapper>
+    <h1>Hello, roman</h1>
+  </div>
 );
