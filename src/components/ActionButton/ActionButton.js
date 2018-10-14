ce9d9fe5bf1a5085103b3e32f3038f575b9bbca9
Adam Romański <adam.romanski@netguru.pl>
2018-10-14 22:42:29 +0200

    Add action button and hide logo

---
+++ src/components/ActionButton/ActionButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ActionButton.module.scss';
+
+export default () => (
+  <div className={styles.circle}>
+    <div className={styles.circleInner} />
+  </div>
+);
