7301779f024a57674a60a80f96b2c80b01c82f3c
Adam <adam.romanski@netguru.pl>
2018-10-14 22:44:50 +0200

    Revert "Finish mobile view for HeroImage"

--- src/components/ActionButton/ActionButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './ActionButton.module.scss';
-
-export default () => (
-  <div className={styles.circle}>
-    <div className={styles.circleInner} />
-  </div>
-);
